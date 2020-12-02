10ec8bd042003f0bc85da8c8f4ceb95891af4838
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-12-02 00:34:51 -0500

    Added basic URLForm and d3 bar chart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react';
+import URLForm from './URLForm.js';
+import BarChart from './BarChart.js';
 
 function App() {
+  const [response, setResponse] = useState("")
+
+  const state = {
+    data: [12, 5, 6, 6, 9, 10],
+    width: 700,
+    height: 500,
+    id: 1
+  }
+  const url = "https://us-central1-faas-297303.cloudfunctions.net/hello_faas_world";
+  axios.get(url).then( res => {
+    setResponse(res.data)
+    console.log(response);
+  }
+  )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Input URL below
         </p>
+  <URLForm></URLForm>
+  <BarChart data={state.data} width={state.width} height={state.height} />
+  <div>{response}</div>
         <a
           className="App-link"
           href="https://reactjs.org"
